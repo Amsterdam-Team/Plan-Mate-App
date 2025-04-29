f1b0578f6aceae9ca38a94b560f7b31753a21c38
Belal Alkhatib <87735826+Belal-Alkhatib@users.noreply.github.com>
2025-04-29 16:07:16 +0300

    remeve unnecessary todo commit

--- src/main/kotlin/logic/usecases/task/CreateTaskUseCase.kt
+++ src/main/kotlin/logic/usecases/task/CreateTaskUseCase.kt
@@ -8,7 +8,6 @@ class CreateTaskUseCase(private val repository: TaskRepository) {
     fun createTask(task: Task): Boolean {
         //todo:
         // 1. check if the project id is exist or not
-        // 2. check if the creation time is not before or after now
         // 3. call create task from TaskRepository
         // 4. return true -> (that is mean there no exceptions thrown)
 
