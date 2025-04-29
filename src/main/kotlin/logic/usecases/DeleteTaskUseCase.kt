e5e1d74c3a2d2c658a465e0e945c62c3982a7c7d
mina0maher <mahermena160@gmail.com>
2025-04-29 12:12:57 +0300

    add empty use case for testing

---
+++ src/main/kotlin/logic/usecases/DeleteTaskUseCase.kt
@@ -0,0 +1,17 @@
+package logic.usecases
+
+import logic.entities.Task
+import logic.exception.PlanMateException
+import logic.repository.LogRepository
+import logic.repository.TaskRepository
+import utils.ResultStatus
+import java.util.UUID
+
+class DeleteTaskUseCase(
+    private val taskRepository: TaskRepository,
+    private val logRepository: LogRepository
+) {
+    fun execute(taskId: UUID, performedBy: String): ResultStatus<Task> {
+        return ResultStatus.Error(PlanMateException.NotFoundException.TaskNotFoundException)
+    }
+}
