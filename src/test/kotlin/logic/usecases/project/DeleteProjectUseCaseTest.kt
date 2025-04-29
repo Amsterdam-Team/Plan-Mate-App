1747844745880dde6c3f6f0fb0730268f69163dd
omer1998 <omerfaris11@gmail.com>
2025-04-29 16:30:05 +0300

    refactor delete project usecase unit tests

--- src/test/kotlin/logic/usecases/project/DeleteProjectUseCaseTest.kt
+++ src/test/kotlin/logic/usecases/project/DeleteProjectUseCaseTest.kt
@@ -1,5 +1,6 @@
 package logic.usecases.project
 
+import com.google.common.truth.Truth.assertThat
 import data.repository.project.ProjectRepositoryImpl
 import io.mockk.*
 import logic.exception.PlanMateException.NotFoundException.*
@@ -9,39 +10,38 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
-class DeleteProjectUseCaseTest{
-  lateinit var repository: ProjectRepositoryImpl
-  lateinit var usecase: DeleteProjectUseCase
+class DeleteProjectUseCaseTest {
+    lateinit var repository: ProjectRepositoryImpl
+    lateinit var usecase: DeleteProjectUseCase
 
-  @BeforeEach
-  fun setUp() {
-   repository = mockk(relaxed = true)
-   usecase = DeleteProjectUseCase(repository)
-  }
-
-     @Test
-     fun `should not throw any exception when deleting project successfully `(){
-
-
-      // when & then
-         assertDoesNotThrow {
-             usecase.deleteProject("er0uj34")
-         }
-      }
-
-
-     @Test
-     fun `should throw not found exception when deleting project not exist`(){
-
-         // when & then
-         assertThrows<ProjectNotFoundException> {
-             usecase.deleteProject("")
-         }
-
-     }
+    @BeforeEach
+    fun setUp() {
+        repository = mockk(relaxed = true)
+        usecase = DeleteProjectUseCase(repository)
+    }
 
     @Test
-    fun `should throw Admin Privileges Required Exception when deleting by user not admin`(){
+    fun `should return true when deleting project successfully `() {
+        // when
+        val result = usecase.deleteProject("er0uj34")
+
+        // then
+        assertThat(result).isTrue()
+    }
+
+
+    @Test
+    fun `should throw not found exception when deleting project not exist`() {
+
+        // when & then
+        assertThrows<ProjectNotFoundException> {
+            usecase.deleteProject("")
+        }
+
+    }
+
+    @Test
+    fun `should throw Admin Privileges Required Exception when deleting by user not admin`() {
 
         // when & then
         assertThrows<AdminPrivilegesRequiredException> {
@@ -51,4 +51,4 @@ class DeleteProjectUseCaseTest{
     }
 
 
- }
+}
