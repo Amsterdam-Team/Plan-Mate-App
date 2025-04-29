78ca10683c5704e20acb8fc76f8bd12b08eeadc9
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2025-04-29 11:49:42 +0300

    fix CreateTaskTestFactory UUID parsing error with valid UUID format

--- src/test/kotlin/logic/usecases/task/testFactory/CreateTaskTestFactory.kt
+++ src/test/kotlin/logic/usecases/task/testFactory/CreateTaskTestFactory.kt
@@ -5,7 +5,7 @@ import java.util.*
 
 object CreateTaskTestFactory {
 
-    private val existingProjectID = UUID.fromString("anExistingProjectID")
+    private val existingProjectID = UUID.fromString("123e4567-e89b-12d3-a456-426614174000")
 
     private const val EXISTING_STATE = "TO-DO"
     val validTask = createTask(name = "Add Create Task Feature", projectID = existingProjectID, state = EXISTING_STATE)
