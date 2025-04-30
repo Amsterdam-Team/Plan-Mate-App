851d62fd46302f5f295edad137259a0b151f32c6
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2025-04-30 16:40:55 +0300

    add AdminMenuHandler and MateMenuHandler using shared baseMenuStart utility

--- src/main/kotlin/ui/menuHandler/AdminMenuHandler.kt
+++ src/main/kotlin/ui/menuHandler/AdminMenuHandler.kt
@@ -1,4 +1,41 @@
 package ui.menuHandler
 
-class AdminMenuHandler {
+import ui.controller.BaseUIController
+import ui.utils.baseMenuStart
+
+
+class AdminMenuHandler(private val adminFeatureControllers: Map<Int, BaseUIController>) {
+    fun start() {
+        baseMenuStart(
+            showMenu = ::showAdminMenu,
+            featureControllers = adminFeatureControllers
+        )
+    }
+
+    private fun showAdminMenu() {
+        println(
+            """
+    ================================
+    Admin Main Menu
+    ================================
+    1. View All Projects
+    2. View State
+    3. Create Task
+    4. Edit Task
+    5. Delete Task
+    6. View Tasks
+    7. View Project Audit History
+    8. View Task Audit History
+    9. Create Mate User
+    10. Create Project
+    11. Edit Project
+    12. Delete Project
+    13. Create State
+    14. Edit State
+    15. Delete State
+    0. Exit
+    """.trimIndent()
+        )
+    }
+
 }
