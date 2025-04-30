5689c32310c8086d314a8f2219baa3f415990264
Nada AL Jarrah <nadajarrahfl20@gmail.com>
2025-04-30 17:10:06 +0200

    feat(logging): add unified logging system for all domain events

---
+++ src/test/logic/helpers/TestObjectCreator.kt
@@ -0,0 +1,31 @@
+package logic.helpers
+
+import logic.entities.Project
+import logic.entities.Task
+import logic.entities.User
+import java.util.*
+
+fun createProject(
+    id: UUID,
+    name: String,
+    states: List<String>,
+    tasks: List<Task>
+): Project {
+    return Project(
+        id = id,
+        name = name,
+        states = states,
+        tasks = tasks
+    )
+}
+fun createUser(
+    id: UUID = UUID.randomUUID(),
+    isAdmin: Boolean = false,
+    username: String = "user1",
+    password: String = "123456"
+): User = User(
+    id = id,
+    isAdmin = isAdmin,
+    username = username,
+    password = password
+)
