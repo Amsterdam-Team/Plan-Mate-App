84c39d6aa34e9c7fb626824f912f965896f36c1f
omer1998 <omerfaris11@gmail.com>
2025-04-30 17:06:19 +0300

    add unit tests for edit task ui controller

--- src/test/kotlin/ui/EditTaskUiControllerTest.kt
+++ src/test/kotlin/ui/EditTaskUiControllerTest.kt
@@ -1,4 +1,35 @@
 package ui
 
+import io.mockk.mockk
+import io.mockk.verify
+import logic.usecases.project.DeleteProjectUseCase
+import logic.usecases.task.EditTaskUseCase
 import org.junit.jupiter.api.Assertions.*
- class EditTaskUiControllerTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import ui.console.ConsoleIOImpl
+
+class EditTaskUiControllerTest {
+    lateinit var useCase: EditTaskUseCase
+    lateinit var uiController: EditTaskUiController
+    lateinit var consoleIO: ConsoleIOImpl
+
+    @BeforeEach
+    fun setUp() {
+        useCase = mockk(relaxed = true)
+        consoleIO = mockk(relaxed = true)
+        uiController = EditTaskUiController(useCase, consoleIO)
+    }
+
+    @Test
+    fun `should take input from user when execute`() {
+        uiController.execute()
+        verify { consoleIO.readFromUser() }
+    }
+
+    @Test
+    fun `should print result to user when complete executing`() {
+        uiController.execute()
+        verify { consoleIO.println(any()) }
+    }
+}
