2cbfff6d161364f42935c2e369e09c03ca486cfe
3bnaby <mahmoud.mohamad68@gmail.com>
2025-04-30 09:53:34 +0300

    add test cases for create project use case

---
+++ src/test/kotlin/logic/usecases/project/CreateProjectUseCaseTest.kt
@@ -0,0 +1,54 @@
+package logic.usecases.project
+
+import com.google.common.truth.Truth.assertThat
+import io.mockk.mockk
+import logic.exception.PlanMateException.ValidationException.*
+import logic.repository.ProjectRepository
+import logic.usecases.testFactories.CreateProjectTestFactory.emptyProjectNameTest
+import logic.usecases.testFactories.CreateProjectTestFactory.emptyProjectStateTest
+import logic.usecases.testFactories.CreateProjectTestFactory.inValidProjectNameTest
+import logic.usecases.testFactories.CreateProjectTestFactory.validProjectTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class CreateProjectUseCaseTest {
+    private lateinit var repository: ProjectRepository
+    private lateinit var useCase: CreateProjectUseCase
+
+    @BeforeEach
+    fun start() {
+        repository = mockk(relaxed = true)
+        useCase = CreateProjectUseCase(repository)
+    }
+
+    @Test
+    fun `should create project when all inputs are valid`() {
+
+        assertThat(useCase.createProject(validProjectTest)).isTrue()
+    }
+
+    @Test
+    fun `should throw EmptyProjectNameException when input name is empty`() {
+
+        assertThrows<EmptyProjectNameException> {
+            useCase.createProject(emptyProjectNameTest)
+        }
+    }
+
+    @Test
+    fun `should throw InvalidProjectNameException when input name is invalid`() {
+
+        assertThrows<InvalidProjectNameException> {
+            useCase.createProject(inValidProjectNameTest)
+        }
+    }
+
+    @Test
+    fun `should throw EmptyProjectStateException when input states is empty`() {
+
+        assertThrows<EmptyProjectStatesException> {
+            useCase.createProject(emptyProjectStateTest)
+        }
+    }
+}
