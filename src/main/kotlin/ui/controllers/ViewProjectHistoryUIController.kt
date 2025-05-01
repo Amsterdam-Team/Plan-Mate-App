ceac22409d6d6bdf993b90be1f327ac009d961ba
mina0maher <mahermena160@gmail.com>
2025-05-01 16:18:33 +0300

    implement UI controller

--- src/main/kotlin/ui/controllers/ViewProjectHistoryUIController.kt
+++ src/main/kotlin/ui/controllers/ViewProjectHistoryUIController.kt
@@ -1,14 +1,34 @@
 package ui.controllers
 
+import logic.entities.LogItem
 import logic.repository.LogRepository
 import logic.repository.ProjectRepository
 import logic.usecases.ViewProjectHistoryUseCase
+import utils.printSwimlanesView
 
 class ViewProjectHistoryUIController(
-    viewProjectHistoryUseCase: ViewProjectHistoryUseCase,
-    projectRepository: ProjectRepository,
+    private val viewProjectHistoryUseCase: ViewProjectHistoryUseCase,
+    private val projectRepository: ProjectRepository,
 ) {
     fun start(){
-
+        println("hi, please enter the id of the project")
+        projectRepository.getProjects().forEach {
+            printSwimlanesView(project = it )
+        }
+        viewProjectHistoryUseCase.execute(readlnOrNull()).forEach {
+            printLogItem(it)
+        }
+    }
+    private fun printLogItem(log: LogItem) {
+        println(
+            """
+        ------------------------------
+        🆔 Log ID     : ${log.id}
+        📝 Message    : ${log.message}
+        📅 Date       : ${log.date}
+        🔗 Entity ID  : ${log.entityId}
+        ------------------------------
+        """.trimIndent()
+        )
     }
 }
