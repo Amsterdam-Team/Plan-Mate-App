a3180c68c092c731a24aeefa62f989d3552fdbc6
omer1998 <omerfaris11@gmail.com>
2025-05-02 01:54:42 +0300

    complete ui controller for delet project

--- src/main/kotlin/ui/DeleteProjectUiController.kt
+++ src/main/kotlin/ui/DeleteProjectUiController.kt
@@ -2,8 +2,29 @@ package ui
 
 import logic.usecases.project.DeleteProjectUseCase
 import ui.console.ConsoleIO
+import ui.controller.BaseUIController
+import ui.utils.tryToExecute
 
-class DeleteProjectUiController(val deleteProjectUseCase: DeleteProjectUseCase, val consoleIO: ConsoleIO) {
+class DeleteProjectUiController(val deleteProjectUseCase: DeleteProjectUseCase, val consoleIO: ConsoleIO): BaseUIController {
+    override fun execute() {
+        tryToExecute(
+           action =  {deleteProjectUseCase.deleteProject(projectId = getProjectId())},
+            onSuccess = {onSuccess()}
+        )
+    }
 
-    fun execute() {}
+    private fun getProjectId(): String{
+        consoleIO.println("please enter project id here: ")
+        val projectId = consoleIO.readFromUser()
+        while (true){
+            if (projectId.isNotBlank() && projectId.isNotEmpty()){
+                return projectId
+            }
+        }
+    }
+    private fun onSuccess(){
+        consoleIO.println("Project deleted successfully")
+    }
+
+
 }
