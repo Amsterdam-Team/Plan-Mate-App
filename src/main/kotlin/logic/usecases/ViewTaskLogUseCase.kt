dfe1e14b2e7c22ac5b4656f21fa28e62ef7cd577
HendSayed25 <hendsayed853@gmail.com>
2025-05-02 13:24:24 +0300

    improve validate function of UUID

--- src/main/kotlin/logic/usecases/ViewTaskLogUseCase.kt
+++ src/main/kotlin/logic/usecases/ViewTaskLogUseCase.kt
@@ -1,7 +1,6 @@
 package logic.usecases
 
 import logic.entities.LogItem
-import logic.exception.PlanMateException
 import logic.exception.PlanMateException.ValidationException.InvalidTaskIDException
 import logic.repository.LogRepository
 import java.util.UUID
@@ -11,17 +10,16 @@ class ViewTaskLogsUseCase(
 ) {
 
     fun viewTaskLogs(taskId : String) : List<LogItem>{
-        val isValid = isValidUUID(taskId)
-        if(!isValid) throw InvalidTaskIDException
-
-        val logs = logRepository.viewLogsById(UUID.fromString(taskId))
+        val uuid = validateUUIDFormat(taskId)
+        val logs = logRepository.viewLogsById(uuid)
         return logs
     }
 
-    private fun isValidUUID(uuidString: String): Boolean {
-        val uuidRegex = Regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$")
-        return uuidRegex.matches(uuidString)
+    private fun validateUUIDFormat(uuidString: String): UUID {
+        return try {
+            UUID.fromString(uuidString.trim())
+        } catch (e: IllegalArgumentException) {
+            throw InvalidTaskIDException
+        }
     }
-
-
 }
