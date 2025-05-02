4eb73b251f53051282063196d66432be2a397deb
Nada AL Jarrah <nadajarrahfl20@gmail.com>
2025-05-02 12:38:58 +0200

    add some changes on unified logging system for all domain events

--- src/main/kotlin/logic/logging/LogEventExtention.kt
+++ src/main/kotlin/logic/logging/LogEventExtention.kt
@@ -1,18 +1,18 @@
 package logic.logging
 
-import kotlinx.datetime.TimeZone
 import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import logic.entities.LogItem
 import java.util.*
 
-fun LogEvent.toLogItem(): LogItem {
+fun LogEvent.toLogItem(userId: UUID?): LogItem {
     val message = when (this) {
         is LogEvent.ProjectCreated ->
             "Project created successfully with ID: '${projectId}' "
 
         is LogEvent.ProjectNameUpdated ->
-            "Project name updated from '${oldName}' to '${newName}' for project: '${projectId}'"
+            "Project name updated from '${oldName}' to '${newName}' for project: '${projectId} by '$userId'"
 
         LogEvent.InvalidActionAttempted ->
             "An invalid action was attempted."
