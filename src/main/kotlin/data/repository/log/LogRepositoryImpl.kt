f08aad261c2af6c0c6ac0d3468b9a9a3a0f01262
mina0maher <mahermena160@gmail.com>
2025-05-02 08:28:25 +0300

    implement log repository

--- src/main/kotlin/data/repository/log/LogRepositoryImpl.kt
+++ src/main/kotlin/data/repository/log/LogRepositoryImpl.kt
@@ -1,12 +1,16 @@
 package data.repository.log
 
+import data.datasources.CsvDataSource
 import logic.entities.LogItem
+import logic.exception.PlanMateException.DataSourceException.EmptyDataException
 import logic.repository.LogRepository
 import java.util.UUID
 
-class LogRepositoryImpl: LogRepository {
+class LogRepositoryImpl(private val csvDataSource: CsvDataSource<LogItem>): LogRepository {
     override fun viewLogsById(id: UUID): List<LogItem> {
-        TODO("Not yet implemented")
+        val logs = csvDataSource.getById(id) as List<LogItem>
+        if(logs.isNullOrEmpty()) throw EmptyDataException
+        return logs
     }
 
     override fun viewAllLogs(): List<LogItem> {
