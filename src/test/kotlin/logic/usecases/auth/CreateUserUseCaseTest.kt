6d5cb6a78eb76b52ed7a8538cef2344d247c28ac
Mohammad Ashraf <mohamedashrf155@gmail.com>
2025-05-03 14:13:55 +0300

    Add another create user test cases

--- src/test/kotlin/logic/usecases/auth/CreateUserUseCaseTest.kt
+++ src/test/kotlin/logic/usecases/auth/CreateUserUseCaseTest.kt
@@ -1,13 +1,16 @@
 package logic.usecases.auth
 
 import com.google.common.truth.Truth.assertThat
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import logic.exception.PlanMateException.ValidationException.InvalidPasswordException
+import logic.exception.PlanMateException.ValidationException.InvalidUsernameException
 import logic.exception.PlanMateException.ValidationException.UserAlreadyExistsException
 import logic.repository.AuthRepository
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import utils.ResultStatus
 import utils.TestDataFactory.createUser
 
@@ -23,9 +26,10 @@ class CreateUserUseCaseTest {
     }
 
     @Test
-    fun `should create user when username and password are valid`() {
+    fun `should return success when username and password are valid`() {
         // Given
         val user = createUser()
+        every { repository.createUser(any()) } just Runs
 
         // When
         val result = useCase.execute(user.username, user.password, user.isAdmin)
@@ -35,25 +39,58 @@ class CreateUserUseCaseTest {
     }
 
     @Test
-    fun `should throw UserAlreadyExistsException when username already exists`() {
+    fun `should return error when username already exists`() {
         // Given
-        val user = createUser()
+        val user = createUser(username = "mohammad")
+        every { repository.createUser(any()) } throws UserAlreadyExistsException
 
-        // When & Then
-        assertThrows<UserAlreadyExistsException> {
-            useCase.execute(user.username, "any", user.isAdmin)
-        }
+        // When
+        val result = useCase.execute(user.username, user.password, user.isAdmin)
+
+        // Then
+        assertThat(result).isInstanceOf(ResultStatus.Error::class.java)
+        assertThat((result as ResultStatus.Error).exception).isInstanceOf(UserAlreadyExistsException::class.java)
     }
 
     @Test
-    fun `should throw InvalidPasswordException when password is blank`() {
+    fun `should return error when password is blank`() {
         // Given
         val username = "mohammad"
         val blankPassword = "  "
 
-        // When & Then
-        assertThrows<InvalidPasswordException> {
-            useCase.execute(username, blankPassword, false)
-        }
+        // When
+        val result = useCase.execute(username, blankPassword, false)
+
+        // Then
+        assertThat(result).isInstanceOf(ResultStatus.Error::class.java)
+        assertThat((result as ResultStatus.Error).exception).isInstanceOf(InvalidPasswordException::class.java)
+    }
+
+    @Test
+    fun `should return error when username is blank`() {
+        // Given
+        val blankUsername = "  "
+        val password = "123456"
+
+        // When
+        val result = useCase.execute(blankUsername, password, false)
+
+        // Then
+        assertThat(result).isInstanceOf(ResultStatus.Error::class.java)
+        assertThat((result as ResultStatus.Error).exception).isInstanceOf(InvalidUsernameException::class.java)
+    }
+
+    @Test
+    fun `should return error when both username and password are blank`() {
+        // Given
+        val blankUsername = "    "
+        val blankPassword = "    "
+
+        // When
+        val result = useCase.execute(blankUsername, blankPassword, false)
+
+        // Then
+        assertThat(result).isInstanceOf(ResultStatus.Error::class.java)
+        assertThat((result as ResultStatus.Error).exception).isInstanceOf(InvalidUsernameException::class.java)
     }
 }
