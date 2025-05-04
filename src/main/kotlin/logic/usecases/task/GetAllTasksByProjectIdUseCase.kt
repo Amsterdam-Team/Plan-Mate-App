4ca090054d3eed1e387c2bf544319cee4d0c49fa
raouf-kamel <raoufkamel2013@gmail.com>
2025-05-04 16:31:04 +0300

    implement get all tasks by project id  use case

--- src/main/kotlin/logic/usecases/task/GetAllTasksByProjectIdUseCase.kt
+++ src/main/kotlin/logic/usecases/task/GetAllTasksByProjectIdUseCase.kt
@@ -1,13 +1,13 @@
 package logic.usecases.task
 
 import logic.entities.Task
+import logic.exception.PlanMateException.NotFoundException.TaskNotFoundException
 import logic.repository.TaskRepository
 import java.util.*
 
 class GetAllTasksByProjectIdUseCase(private val repository: TaskRepository) {
-    operator fun invoke(projectId: UUID): List<Task> {
-        return repository.getAllTasksByProjectId(projectId)
-
-    }
-
+    operator fun invoke(projectId: UUID): List<Task> =
+        repository.getAllTasksByProjectId(projectId)
+            .filter { it.projectId == projectId }
+            .ifEmpty { throw TaskNotFoundException }
 }
