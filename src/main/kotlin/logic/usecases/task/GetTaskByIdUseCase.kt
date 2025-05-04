43ecb6ef7316767448789a015c347a67119146e8
raouf-kamel <raoufkamel2013@gmail.com>
2025-05-04 16:36:46 +0300

    implement get task by task id  use case

--- src/main/kotlin/logic/usecases/task/GetTaskByIdUseCase.kt
+++ src/main/kotlin/logic/usecases/task/GetTaskByIdUseCase.kt
@@ -1,6 +1,7 @@
 package logic.usecases.task
 
 import logic.entities.Task
+import logic.exception.PlanMateException.NotFoundException.TaskNotFoundException
 import logic.repository.TaskRepository
 import java.util.*
 
@@ -8,9 +9,18 @@ class GetTaskByIdUseCase(
     private val repository: TaskRepository
 ) {
     operator fun invoke(taskId: UUID): Task {
-        return repository.getTaskById(taskId)
+        val task = try {
+            repository.getTaskById(taskId)
+        } catch (e: TaskNotFoundException) {
+            throw e
+        }
+        if (task.id != taskId) {
+            throw TaskNotFoundException
+        }
 
+        return task
     }
 }
+
 
 
