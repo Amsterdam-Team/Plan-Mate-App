6088b1aadefd4a1889be029ae0d3a9666f52e2aa
raouf-kamel <raoufkamel2013@gmail.com>
2025-05-04 17:16:51 +0300

    implement delete state use case

--- src/main/kotlin/logic/usecases/state/DeleteStateUseCase.kt
+++ src/main/kotlin/logic/usecases/state/DeleteStateUseCase.kt
@@ -1,13 +1,25 @@
 package logic.usecases.state
 
+import logic.exception.PlanMateException.NotFoundException.ProjectNotFoundException
+import logic.exception.PlanMateException.ValidationException.InvalidStateNameException
 import logic.repository.ProjectRepository
 import java.util.*
 
-class DeleteStateUseCase(private val repository: ProjectRepository) {
+class DeleteStateUseCase(
+    private val projectRepository: ProjectRepository
+) {
     operator fun invoke(projectId: UUID, oldState: String): Boolean {
-//        throw  ProjectNotFoundException
-        return repository.deleteStateById(projectId, oldState)
+        validateStateName(oldState)
 
+        val project = projectRepository.getProject(projectId) ?: throw ProjectNotFoundException
+
+        return projectRepository.deleteStateById(project.id, oldState)
+    }
+
+
+    private fun validateStateName(state: String) {
+        if (state.isBlank()) throw InvalidStateNameException
     }
 }
+
 
