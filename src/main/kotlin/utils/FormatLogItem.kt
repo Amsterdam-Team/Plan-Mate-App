bf190d5224b934cdf32b75b59f1f01c4df14d942
mina0maher <mahermena160@gmail.com>
2025-05-05 17:11:23 +0300

    Remove printing ids in log

--- src/main/kotlin/utils/FormatLogItem.kt
+++ src/main/kotlin/utils/FormatLogItem.kt
@@ -5,9 +5,7 @@ import logic.entities.LogItem
 fun formatLogItem(log: LogItem) =
     """
         ------------------------------
-        🆔 Log ID     : ${log.id}
         📝 Message    : ${log.message}
         📅 Date       : ${log.date}
-        🔗 Entity ID  : ${log.entityId}
         ------------------------------
         """.trimIndent()
