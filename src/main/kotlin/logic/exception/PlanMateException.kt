5e6e62ab129a01a937462c1da04c92bea8bca7e5
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-05-05 08:04:24 +0300

    Added InvalidUUIDFormat in Exception Class

--- src/main/kotlin/logic/exception/PlanMateException.kt
+++ src/main/kotlin/logic/exception/PlanMateException.kt
@@ -19,6 +19,7 @@ sealed class PlanMateException : Exception() {
         data object InvalidProjectNameException : ValidationException()
         data object InvalidProjectIDException : ValidationException()
         data object EmptyDataException : ValidationException()
+        data object InvalidUUIDFormatException:ValidationException()
 
         data object EmptyProjectStatesException : ValidationException()
         data object EmptyProjectNameException : ValidationException()
