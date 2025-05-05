514d56ad51450dfa38505b3a0fdeb5438e163377
omer1998 <omerfaris11@gmail.com>
2025-05-06 02:46:32 +0300

    add validation logic for user if admin or not

--- src/main/kotlin/logic/usecases/state/AddStateUseCase.kt
+++ src/main/kotlin/logic/usecases/state/AddStateUseCase.kt
@@ -1,6 +1,8 @@
 package logic.usecases.state
 
+import logic.entities.User
 import logic.exception.PlanMateException
+import logic.exception.PlanMateException.AuthorizationException.AdminPrivilegesRequiredException
 import logic.exception.PlanMateException.NotFoundException.ProjectNotFoundException
 import logic.exception.PlanMateException.ValidationException.EmptyDataException
 import logic.exception.PlanMateException.ValidationException.InvalidProjectIDException
@@ -13,13 +15,15 @@ import java.util.UUID
 
 class AddStateUseCase(
     private val repository: ProjectRepository,
-    private val validateInputUseCase: ValidateInputUseCase
+    private val validateInputUseCase: ValidateInputUseCase,
+    private val user: User
 ) {
 
     fun execute(projectId: String, state: String): Boolean{
         if(!validateInputUseCase.isValidName(state)) throw InvalidStateNameException
         if(projectId.isEmpty() || projectId.isBlank()) throw EmptyDataException
         if(!validateInputUseCase.isValidUUID(projectId)) throw InvalidProjectIDException
+        if(!user.isAdmin) throw AdminPrivilegesRequiredException
         return repository.addStateById(UUID.fromString(projectId), state)
 
     }
