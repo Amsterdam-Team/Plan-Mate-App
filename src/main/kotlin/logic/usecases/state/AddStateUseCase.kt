bb040ef721d6116a0dabf66319a4094ffcb76076
omer1998 <omerfaris11@gmail.com>
2025-05-05 22:07:38 +0300

    refactor validation process

--- src/main/kotlin/logic/usecases/state/AddStateUseCase.kt
+++ src/main/kotlin/logic/usecases/state/AddStateUseCase.kt
@@ -3,6 +3,7 @@ package logic.usecases.state
 import logic.exception.PlanMateException
 import logic.exception.PlanMateException.NotFoundException.ProjectNotFoundException
 import logic.exception.PlanMateException.ValidationException.EmptyDataException
+import logic.exception.PlanMateException.ValidationException.InvalidProjectIDException
 import logic.exception.PlanMateException.ValidationException.InvalidStateNameException
 import logic.exception.PlanMateException.ValidationException.InvalidTaskIDException
 import logic.repository.ProjectRepository
@@ -16,23 +17,11 @@ class AddStateUseCase(
 ) {
 
     fun execute(projectId: String, state: String): Boolean{
-        if(!validateInputUseCase.isValidName(state)){
-            println("state name must not be empty, it can contain underscore, hyphen, digits but not special characters")
-        }
-        validateId(projectId)
+        if(!validateInputUseCase.isValidName(state)) throw InvalidStateNameException
+        if(projectId.isEmpty() || projectId.isBlank()) throw EmptyDataException
+        if(!validateInputUseCase.isValidUUID(projectId)) throw InvalidProjectIDException
         return repository.addStateById(UUID.fromString(projectId), state)
 
     }
 
-    fun validateId(id:String){
-        if (id.isEmpty() || id.isBlank()){
-            throw EmptyDataException
-        }
-
-        try {
-            UUID.fromString(id)
-        } catch (e: IllegalArgumentException){
-            throw InvalidTaskIDException
-        }
-    }
 }
--- src/main/kotlin/ui/utils/UIHelperFunctions.kt
+++ src/main/kotlin/ui/utils/UIHelperFunctions.kt
@@ -37,7 +37,7 @@ fun getErrorMessageByException(exception: Exception): String {
 
         is InvalidPasswordException -> "The password you entered is not valid. Please follow the password rules."
 
-        is InvalidStateNameException -> "The state name is not valid. State name must be not be empty and does not contain any numbers or non alphabet characters"
+        is InvalidStateNameException -> "The state name is not valid. State name must be not be empty and contain at least 3 characters"
 
         is InvalidTaskNameException -> "The task name is not valid. Please enter a proper task name."
 
