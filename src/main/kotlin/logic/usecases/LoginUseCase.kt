90717e4f21d94f0be0be8a9d4a575b037bc74614
HendSayed25 <hendsayed853@gmail.com>
2025-05-05 17:31:23 +0300

    made password hashed in usecase

--- src/main/kotlin/logic/usecases/LoginUseCase.kt
+++ src/main/kotlin/logic/usecases/LoginUseCase.kt
@@ -3,13 +3,14 @@ package logic.usecases
 import logic.entities.User
 import logic.exception.PlanMateException
 import logic.repository.AuthRepository
+import ui.utils.md5Hash
 
 class LoginUseCase(
     private val authRepository : AuthRepository
 ) {
 
     fun validateUserCredentials(username : String, password : String): User{
-        val userData = authRepository.login(username,password)
+        val userData = authRepository.login(username, md5Hash(password))
         return userData
     }
 }
