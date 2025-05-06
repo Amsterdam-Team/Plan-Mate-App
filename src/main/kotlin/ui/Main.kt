c3cfe2e72632c4885b544cadc4a4e5e3f8392557
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-05-06 19:28:17 +0300

    Update Main.kt

--- src/main/kotlin/ui/Main.kt
+++ src/main/kotlin/ui/Main.kt
@@ -1,8 +1,5 @@
 package ui
 
-import data.datasources.MongoDatabaseFactory
-import data.datasources.userDataSource.UserDataSourceInterface
-import data.datasources.userDataSource.UserRemoteDataSource
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
@@ -11,17 +8,10 @@ import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import org.koin.core.component.inject
-
-import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.json.Json
-import logic.entities.User
 import logic.usecases.LoginUseCase
 import org.koin.core.context.startKoin
 import org.koin.java.KoinJavaComponent.getKoin
 import ui.console.ConsoleIO
-import org.koin.ktor.ext.inject
-
 import ui.controller.CreateTaskUIController
 import ui.controllers.CreateProjectUIController
 import ui.controllers.UpdateStateUiController
@@ -30,50 +20,47 @@ import ui.menuHandler.MateMenuHandler
 import ui.project.GetProjectUIController
 import ui.project.ViewProjectHistoryUIController
 import java.util.*
-import java.util.logging.Level
-import java.util.logging.LogManager
-import java.util.logging.Logger
 
 
 fun main() {
-//    startKoin{
-//        modules(
-//            appModule
-//        )
-//    }
-//
-//    val consoleIO : ConsoleIO = getKoin().get()
-//    val loginUseCase: LoginUseCase = getKoin().get()
-//    val createProjectUIController: CreateProjectUIController = getKoin().get()
-//    val deleteTaskUiController: DeleteProjectUiController = getKoin().get()
-//    val viewProjectHistoryUIController: ViewProjectHistoryUIController = getKoin().get()
-//    val updateStateUiController: UpdateStateUiController = getKoin().get()
-//    val getProjectUIController: GetProjectUIController = getKoin().get()
-//    val deleteProjectUiController: DeleteProjectUiController = getKoin().get()
-//    val viewTaskLogsUIController: ViewTaskLogsUIController = getKoin().get()
-//    val createTaskUIController: CreateTaskUIController = getKoin().get()
-//
-//
-//
-//    val adminHandler: AdminMenuHandler = AdminMenuHandler(
-//        mapOf(
-//            1 to createProjectUIController,
-//            2 to updateStateUiController,
-//            12 to deleteProjectUiController
-//        )
-//    )
-//
-//    val mateHandler: MateMenuHandler = MateMenuHandler(
-//        mapOf(
-//            1 to getProjectUIController,
-//        )
-//    )
-//
-//
-//
-//    val loginUIController = LoginUIController(loginUseCase, adminHandler, mateHandler, consoleIO)
-//
-//    loginUIController.execute()
+    startKoin{
+        modules(
+            appModule
+        )
+    }
+
+    val consoleIO : ConsoleIO = getKoin().get()
+    val loginUseCase: LoginUseCase = getKoin().get()
+    val createProjectUIController: CreateProjectUIController = getKoin().get()
+    val deleteTaskUiController: DeleteProjectUiController = getKoin().get()
+    val viewProjectHistoryUIController: ViewProjectHistoryUIController = getKoin().get()
+    val updateStateUiController: UpdateStateUiController = getKoin().get()
+    val getProjectUIController: GetProjectUIController = getKoin().get()
+    val deleteProjectUiController: DeleteProjectUiController = getKoin().get()
+    val viewTaskLogsUIController: ViewTaskLogsUIController = getKoin().get()
+    val createTaskUIController: CreateTaskUIController = getKoin().get()
+
+
+
+    val adminHandler: AdminMenuHandler = AdminMenuHandler(
+        mapOf(
+            1 to createProjectUIController,
+            2 to updateStateUiController,
+            12 to deleteProjectUiController
+        )
+    )
+
+    val mateHandler: MateMenuHandler = MateMenuHandler(
+        mapOf(
+            1 to getProjectUIController,
+        )
+    )
+
+
+
+    val loginUIController = LoginUIController(loginUseCase, adminHandler, mateHandler, consoleIO)
+
+    loginUIController.execute()
 
 
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
