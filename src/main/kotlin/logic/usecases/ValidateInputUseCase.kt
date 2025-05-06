4eed59789272bfdb566c82c23cbd250e7645fbf1
3bnaby <mahmoud.mohamad68@gmail.com>
2025-05-06 14:37:00 +0300

    refactor create project use case logic

--- src/main/kotlin/logic/usecases/ValidateInputUseCase.kt
+++ src/main/kotlin/logic/usecases/ValidateInputUseCase.kt
@@ -1,12 +1,10 @@
 package logic.usecases
 
-import java.util.UUID
+import java.util.*
 
 class ValidateInputUseCase {
     fun isValidName(name: String): Boolean {
-        return name.isNotBlank() &&
-                name.length in 3..100 &&
-                name.matches(Regex("^[\\w\\s-]+$"))
+        return name.isNotBlank() && name.length in 3..100 && name.matches(Regex("^[\\w\\s-]+$"))
     }
 
     fun isValidUUID(uuid: String): Boolean = try {
@@ -14,5 +12,12 @@ class ValidateInputUseCase {
         true
     } catch (_: IllegalArgumentException) {
         false
+    }
+
+    fun isValidProjectStates(states: List<String>): Boolean {
+        if (states.isEmpty()) return false
+        return states.all { state ->
+            state.isNotBlank() && state.matches(Regex("^[a-zA-Z]+( [a-zA-Z]+)*$"))
+        }
     }
 }
