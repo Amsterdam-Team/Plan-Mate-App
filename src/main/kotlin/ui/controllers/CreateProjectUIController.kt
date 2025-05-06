819e1d62510fefdcb21141f639f58cb744ce8852
3bnaby <mahmoud.mohamad68@gmail.com>
2025-05-06 14:57:31 +0300

    refactor create project UI logic

--- src/main/kotlin/ui/controllers/CreateProjectUIController.kt
+++ src/main/kotlin/ui/controllers/CreateProjectUIController.kt
@@ -1,26 +1,54 @@
 package ui.controllers
 
-import logic.entities.Project
+import console.ConsoleIO
 import logic.usecases.project.CreateProjectUseCase
 import ui.controller.BaseUIController
 import ui.utils.tryToExecute
-import java.util.UUID
 
-class CreateProjectUIController(private val createProjectUseCase: CreateProjectUseCase) : BaseUIController {
+class CreateProjectUIController(
+    private val createProjectUseCase: CreateProjectUseCase, private val consoleIO: ConsoleIO
+) : BaseUIController {
+
     override fun execute() {
-        tryToExecute(
-            action = ::startCreatingAProject, onSuccess = { project -> createProjectUseCase.createProject(project) })
-    }
 
-    private fun startCreatingAProject(): Project {
-        println("Please enter project name")
-        val projectName = readlnOrNull().toString()
-        println("Enter project states (comma-separated):")
-        val state = readLine() ?: ""
+        consoleIO.println(PROJECT_NAME_PROMPT_MESSAGE)
+        val projectName = consoleIO.readFromUser()
+        consoleIO.println(PROJECT_STATES_PROMPT_MESSAGE)
+        val state = consoleIO.readFromUser()
         val projectStates = state.split(",").map { it.trim() }
 
-        return Project(
-            id = UUID.randomUUID(), name = projectName, states = projectStates, tasks = emptyList()
+        tryToExecute(
+            action = { createProjectUseCase.createProject(name = projectName, states = projectStates) },
+            onSuccess = ::onCreateProjectSuccess,
+            onError = ::onCreateProjectFail
         )
+    }
+
+    private fun onCreateProjectSuccess(isCreatedSuccessfully: Boolean) {
+        if (isCreatedSuccessfully) {
+            consoleIO.println(PROJECT_CREATED_SUCCESSFULLY_MESSAGE)
+            return
+        }
+    }
+
+    private fun onCreateProjectFail(exception: Exception) {
+        consoleIO.println(FAIL_TO_CREATE_PROJECT_MESSAGE)
+
+        val input = consoleIO.readFromUser().trim().uppercase()
+        when (input) {
+            RETRY.uppercase() -> execute()
+            CANCEL.uppercase() -> return
+            else -> onCreateProjectFail(exception)
+        }
+    }
+
+
+    private companion object {
+        const val PROJECT_NAME_PROMPT_MESSAGE = "Please enter project name"
+        const val PROJECT_STATES_PROMPT_MESSAGE = "Enter project states (comma-separated):"
+        const val PROJECT_CREATED_SUCCESSFULLY_MESSAGE = "✅ Your project has been Created Successfully"
+        const val RETRY = "retry"
+        const val CANCEL = "cancel"
+        const val FAIL_TO_CREATE_PROJECT_MESSAGE = "❌ Please Enter All Inputs Correctly, inter $RETRY or $CANCEL"
     }
 }
