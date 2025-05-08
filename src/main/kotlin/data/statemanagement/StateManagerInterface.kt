13fd7f565fa30c8b785901a6de63cdd84c22ca62
mina0maher <mahermena160@gmail.com>
2025-05-08 15:11:01 +0300

    Setup state manager interface

---
+++ src/main/kotlin/data/statemanagement/StateManagerInterface.kt
@@ -0,0 +1,14 @@
+package data.statemanagement
+
+import logic.entities.Project
+import logic.entities.Task
+import logic.entities.User
+
+interface StateManagerInterface {
+    fun getCurrentUser(): User
+    fun setCurrentUser(user:User)
+    fun getCurrentProject(): Project
+    fun setCurrentProject(project: Project)
+    fun getCurrentTask(): Task
+    fun setCurrentTask(task: Task)
+}
