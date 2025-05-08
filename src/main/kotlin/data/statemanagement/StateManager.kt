732aab8bb7815138721f6dfc906e4c399969db48
mina0maher <mahermena160@gmail.com>
2025-05-08 15:19:33 +0300

    Add state manager Implementation

--- src/main/kotlin/data/statemanagement/StateManager.kt
+++ src/main/kotlin/data/statemanagement/StateManager.kt
@@ -4,11 +4,39 @@ import logic.entities.Project
 import logic.entities.Task
 import logic.entities.User
 
-interface StateManager {
-    fun getCurrentUser(): User
-    fun setCurrentUser(user:User)
-    fun getCurrentProject(): Project
-    fun setCurrentProject(project: Project)
-    fun getCurrentTask(): Task
-    fun setCurrentTask(task: Task)
+object StateManager: StateManagerInterface {
+    private var currentUser: User? = null
+    private var currentProject: Project? = null
+    private var currentTask: Task? = null
+
+    override fun getCurrentUser(): User {
+        return currentUser ?: throw Exception("No current user")
+    }
+
+    override fun setCurrentUser(user: User) {
+        currentUser = user
+    }
+
+    override fun getCurrentProject(): Project {
+        return currentProject ?: throw Exception("No current user")
+    }
+
+    override fun setCurrentProject(project: Project) {
+        currentProject = project
+    }
+
+    override fun getCurrentTask(): Task {
+        return currentTask ?: throw Exception("No current user")
+
+    }
+
+    override fun setCurrentTask(task: Task) {
+        currentTask = task
+    }
+
+    override fun logOut() {
+        currentUser = null
+        currentProject = null
+        currentTask = null
+    }
 }
