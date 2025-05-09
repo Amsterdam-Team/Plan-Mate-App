3913d2afd1d5610ce908924f6d1fba1c3dc9ae31
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-05-09 13:58:21 +0300

    Implemented Task Remote Datasource

--- src/main/kotlin/data/datasources/taskDataSource/TaskDataSource.kt
+++ src/main/kotlin/data/datasources/taskDataSource/TaskDataSource.kt
@@ -1,45 +1,101 @@
 package data.datasources.taskDataSource
 
+import com.mongodb.client.model.Filters
+import com.mongodb.client.model.Updates
 import com.mongodb.kotlin.client.coroutine.MongoCollection
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.toList
 import logic.entities.Task
+import logic.exception.PlanMateException.DataSourceException.ObjectDoesNotExistException
+import org.bson.Document
 import java.util.*
 
 class TaskDataSource(
-    tasksCollection: MongoCollection<Task>
+    private val tasksCollection: MongoCollection<Task>
 ): ITaskDataSource {
     override suspend fun getAllTasks(): List<Task> {
-        TODO("Not yet implemented")
+        return tasksCollection.find().toList()
     }
 
     override suspend fun getAllProjectTasks(projectId: UUID): List<Task> {
-        TODO("Not yet implemented")
+        return tasksCollection.find(Filters.eq("projectId", projectId)).toList()
     }
 
     override suspend fun getTaskById(taskId: UUID): Task {
-        TODO("Not yet implemented")
+        return tasksCollection.find(Filters.eq("id", taskId)).firstOrNull() ?: throw ObjectDoesNotExistException
     }
 
     override suspend fun insertTask(task: Task): Boolean {
-        TODO("Not yet implemented")
+        val existingTask = tasksCollection.find(
+            Filters.or(
+                Filters.eq("id", task.id),
+                Filters.and(
+                    Filters.eq("projectId", task.projectId),
+                    Filters.eq("name", task.name)
+                )
+            )
+        ).firstOrNull()
+
+        if (existingTask != null) return false
+
+        return tasksCollection.insertOne(task).wasAcknowledged()
     }
 
     override suspend fun deleteTask(taskId: UUID): Boolean {
-        TODO("Not yet implemented")
+        val result = tasksCollection.deleteOne(Filters.eq("id", taskId))
+        return result.deletedCount > 0
     }
 
     override suspend fun getTaskState(taskId: UUID): String {
-        TODO("Not yet implemented")
+        val task = tasksCollection.find(Filters.eq("id", taskId)).firstOrNull() ?: throw ObjectDoesNotExistException
+        return task.state
     }
 
     override suspend fun updateTaskName(taskId: UUID, newName: String): Boolean {
-        TODO("Not yet implemented")
+        val task = tasksCollection.find(Filters.eq("id", taskId)).firstOrNull() ?: return false
+        val duplicateInSameProject = tasksCollection.find(
+            Filters.and(
+                Filters.eq("projectId", task.projectId),
+                Filters.eq("name", newName),
+                Filters.ne("id", taskId)
+            )
+        ).firstOrNull()
+
+
+        if (duplicateInSameProject != null) return false
+
+        val result = tasksCollection.updateOne(
+            Filters.eq("id", taskId),
+            Updates.set("name", newName)
+        )
+
+        return result.modifiedCount > 0
     }
 
     override suspend fun updateTaskState(taskId: UUID, newState: String): Boolean {
-        TODO("Not yet implemented")
+        val result = tasksCollection.updateOne(
+            Filters.eq("id", taskId),
+            Updates.set("state", newState)
+        )
+        return result.modifiedCount > 0
     }
 
     override suspend fun replaceAllTasks(tasks: List<Task>): Boolean {
-        TODO("Not yet implemented")
+
+        val uniqueIds = tasks.map { it.id }.toSet()
+        if (uniqueIds.size != tasks.size) return false
+
+        val hasDuplicateNameInProject = tasks
+            .groupBy { it.projectId }
+            .values
+            .any { tasksInProject ->
+                tasksInProject.map { it.name }.toSet().size != tasksInProject.size
+            }
+
+        if (hasDuplicateNameInProject) return false
+
+        tasksCollection.deleteMany(Document())
+        tasksCollection.insertMany(tasks)
+        return true
     }
 }
