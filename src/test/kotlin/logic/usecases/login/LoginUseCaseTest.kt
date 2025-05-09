d203dfc84f366fa51b060bc503de5fba198baa6e
Mohammad Ashraf <mohamedashrf155@gmail.com>
2025-05-09 17:59:29 +0300

    Add hashed password md5Hash to test upon it not a normal password without hashing

--- src/test/kotlin/logic/usecases/login/LoginUseCaseTest.kt
+++ src/test/kotlin/logic/usecases/login/LoginUseCaseTest.kt
@@ -12,65 +12,63 @@ import logic.usecases.utils.StateManager
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import ui.utils.md5Hash
 
 class LoginUseCaseTest {
-    private lateinit var repository : AuthRepository
+    private lateinit var repository: AuthRepository
     private lateinit var stateManager: StateManager
-    private lateinit var useCase : LoginUseCase
+    private lateinit var useCase: LoginUseCase
 
 
     @BeforeEach
-    fun setup(){
+    fun setup() {
         repository = mockk(relaxed = true)
         stateManager = mockk(relaxed = true)
-        useCase = LoginUseCase(repository,stateManager)
+        useCase = LoginUseCase(repository, stateManager)
     }
 
     @Test
-    fun `should return full user data when username and password are valid format and found`() = runTest{
-        //Given
-        val username= "Hend"
-        val password = "H123456"
-        //When
-        coEvery { repository.login(username, password) } returns validUserData()
-        //then
-        val returnedUser = useCase.validateUserCredentials(username, password)
-        assertThat(returnedUser).isEqualTo(validUserData())
-    }
+    fun `should return full user data when username and password are valid format and found`() =
+        runTest {
+            //Given
+            val username = "Hend"
+            val password = "H123456"
+            val hashedPassword = md5Hash(password)
+            val expectedUser = validUserData()
+            coEvery { repository.login(username, hashedPassword) } returns expectedUser
+
+            //When
+            val returnedUser = useCase.validateUserCredentials(username, password)
+
+            //then
+            assertThat(returnedUser).isEqualTo(expectedUser)
+        }
 
     @Test
-    fun `should throw wrong username exception when username is wrong`() = runTest{
-        //Given
-        val username= "Hen"
+    fun `should throw wrong username exception when username is wrong`() = runTest {
+        // Given
+        val username = "Hen"
         val password = "H123456"
+        val hashedPassword = md5Hash(password)
 
-        coEvery {
-            repository.login(
-                username,
-                password
-            )
-        } throws WrongUsernameException
+        coEvery { repository.login(username, hashedPassword) } throws WrongUsernameException
 
-        //When && Throw
+        // When & Then
         assertThrows<WrongUsernameException> {
             useCase.validateUserCredentials(username, password)
         }
     }
 
     @Test
-    fun `should throw wrong password exception when password is wrong`()=runTest {
-        //Given
-        val username= "Hend"
-        val password = "H123457"
+    fun `should throw wrong password exception when password is wrong`() = runTest {
+        // Given
+        val username = "Hend"
+        val password = "WrongPass"
+        val hashedPassword = md5Hash(password)
 
-        coEvery {
-            repository.login(
-                username,
-                password
-            )
-        } throws WrongPasswordException
+        coEvery { repository.login(username, hashedPassword) } throws WrongPasswordException
 
-        //When && Throw
+        // When & Then
         assertThrows<WrongPasswordException> {
             useCase.validateUserCredentials(username, password)
         }
