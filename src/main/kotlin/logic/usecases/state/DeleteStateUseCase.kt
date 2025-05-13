c4b5dba6eb0658c62e804c3b7b10e40844b0902d
raouf-kamel <raoufkamel2013@gmail.com>
2025-05-13 15:36:05 +0300

    refactor delete stata use case

--- src/main/kotlin/logic/usecases/state/DeleteStateUseCase.kt
+++ src/main/kotlin/logic/usecases/state/DeleteStateUseCase.kt
@@ -1,12 +1,49 @@
 package logic.usecases.state
 
+import logic.exception.PlanMateException.AuthorizationException.AdminPrivilegesRequiredException
+import logic.exception.PlanMateException.NotFoundException.ProjectNotFoundException
+import logic.exception.PlanMateException.NotFoundException.StateNotFoundException
+import logic.exception.PlanMateException.ValidationException.*
 import logic.repository.IProjectRepository
+import logic.repository.ITaskRepository
+import logic.usecases.logs.LoggerUseCase
+import logic.usecases.utils.StateManager
+import logic.usecases.utils.ValidateInputUseCase
 import java.util.*
 
-class DeleteStateUseCase(private val repository: IProjectRepository) {
-    suspend operator fun invoke(projectId: UUID, oldState: String): Boolean {
-//        throw  ProjectNotFoundException
-        return repository.deleteStateById(projectId, oldState)
+class DeleteStateUseCase(
+    private val repository: IProjectRepository,
+    private val taskRepository: ITaskRepository,
+    private val stateManager: StateManager,
+    private val loggerUseCase: LoggerUseCase,
+    private val validateInputUseCase: ValidateInputUseCase
+) {
+    suspend operator fun invoke(projectId: String, state: String): Boolean {
+        val user = stateManager.getLoggedInUser()
+        if (!user.isAdmin) throw AdminPrivilegesRequiredException
+        if (!validateInputUseCase.isValidName(state)) throw InvalidStateNameException
+        if (!validateInputUseCase.isValidUUID(projectId)) throw InvalidProjectIDException
+
+        val uuid = UUID.fromString(projectId)
+
+        val project = repository.getProjectById(uuid) ?: throw ProjectNotFoundException
+
+        if (!project.states.contains(state)) {
+            throw StateNotFoundException
+        }
+
+        val hasTasks = taskRepository.hasTasksWithState(uuid, state)
+        if (hasTasks) {
+            throw StateHasTasksException
+        }
+
+        val deleted = repository.deleteStateById(uuid, state)
+
+        if (deleted) {
+            loggerUseCase.createLog("Delete State ${state} in Project Name  ${project.name}", uuid)
+        }
+
+        return deleted
 
     }
 }
